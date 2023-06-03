52c94603c9feac45c3cf6ba812d28c51ced87f93
JTushar <tushar@heady.io>
2023-06-03 12:23:51 +0530

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Tushar Jaunjalkar on 03/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
